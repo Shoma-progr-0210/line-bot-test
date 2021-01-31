a65b05d89115539b20bdd7a74ee33196cecdaa74
shoma.h <Shoma.progr.0210@gmail.com>
2021-01-31 22:54:00 +0900

    update: db接続テストを追加

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import psycopg2
 import logging
 import os
 import sys
@@ -25,9 +26,51 @@ app.logger.setLevel(logging.INFO)
 #環境変数取得
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_DBNAME = os.environ["DB_DBNAME"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
+# DBコネクション取得関数
+def get_connection():
+    dsn = "host=" + DB_HOST \
+        + "port=" + str(DB_PORT) \
+        + "dbname=" + DB_DBNAME \
+        + "user=" + DB_USER \
+        + "password=" + DB_PASSWORD
+    
+    return psycopg2.connect(dsn)
+
+def get_response_message(msg_from):
+    # "日付"が入力された時だけDBアクセス
+    if msg_from=="日付":
+        with get_connection() as conn:
+            with conn.cursor(name="cs") as cur:
+                try:
+                    sql_str = "SELECT TO_CHAR(CURRENT_DATE, 'yyyy/mm/dd');"
+                    cur.execute(sql_str)
+                    (reply_msg,) = cur.fetchone()
+                    return reply_msg
+                except:
+                    reply_msg = "exception"
+                    return reply_msg
+    elif msg_from == "ヘルプ" or msg_from == "help":
+        reply_msg = "メニュー:\n" \
+        + "(なし) => オウム返し\n" \
+        + "日付 => 今日の日付を返します\n" \
+        + "カウント or count => 二行目からの文字数を数えます(改行、空白は除きます)"
+        return reply_msg
+    elif msg_from.startswith("カウント\n") or msg_from.startswith("count\n"):
+        txt = msg_from.replace(" ","").replace("　","").replace("\n","").lstrip("カウント").lstrip("count")
+        reply_msg = "文字数は " + str(len(txt)) + " 文字です。"
+        return reply_msg
+    else:
+        # それ以外はオウム返し
+        return reply_msg
 
 @app.route("/")
 def hello_world():
