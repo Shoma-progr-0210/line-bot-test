1a75c3d91a424f36a5896031d752ce992714e60d
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-02 23:05:10 +0900

    update: リマインド登録を追加、models.pyの修正

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 import psycopg2
 import os
+from datetime import datetime, timedelta
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -13,6 +14,7 @@ from linebot.models import (
 )
 
 from reminder.app import app
+from reminder.models.models import Schedule
 
 
 #環境変数取得
@@ -50,22 +52,6 @@ def get_response_message(msg_from):
     #             except:
     #                 reply_msg = "exception"
     #                 return reply_msg
-    if msg_from == "ヘルプ" or msg_from == "help":
-        # ヘルプメニュー
-        reply_msg = "メニュー:\n" \
-        + "(なし) => オウム返し\n" \
-        # + "日付 => 今日の日付を返します\n" \
-        + "カウント or count => 二行目からの文字数を数えます(改行、空白は除きます)"
-        return reply_msg
-    elif msg_from.startswith("カウント\n") or msg_from.startswith("count\n"):
-        # メッセージの文字数カウント
-        txt = msg_from.replace(" ","").replace("　","").replace("\n","").lstrip("カウント").lstrip("count")
-        reply_msg = "文字数は " + str(len(txt)) + " 文字です。"
-        return reply_msg
-    else:
-        # それ以外はオウム返し
-        reply_msg = msg_from
-        return reply_msg
 
 @app.route("/")
 def hello_world():
@@ -90,12 +76,39 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text=get_response_message(event.message.text)))
+    msg_from = event.massage.text
+    if msg_from == "ヘルプ" or msg_from == "help":
+        # ヘルプメニュー
+        reply_msg = "メニュー:\n" \
+        + "(なし) => オウム返し\n" \
+        # + "日付 => 今日の日付を返します\n" \
+        + "カウント or count => 二行目からの文字数を数えます(改行、空白は除きます)"
+    elif msg_from.startswith("カウント\n") or msg_from.startswith("count\n"):
+        # メッセージの文字数カウント
+        txt = msg_from.replace(" ","").replace("　","").replace("\n","").lstrip("カウント").lstrip("count")
+        reply_msg = "文字数は " + str(len(txt)) + " 文字です。"
+    elif msg_from == "登録\n":
+        # 登録
+        # 時間: %Y/%m/%d %H:%M
+        # メッセージ
+        try:
+            profile = line_bot_api.get_profile(event.source.user_id)
+            data = msg_from.split("\n")
+            message = data[2]
+            time = datetime.strptime(data[1], '%Y/%m/%d %H:%M')
+            Schedule.create(profile.user_id, profile.display_name, message, time)
+            reply_msg = "リマインドを登録しました。"
+        except:
+            reply_msg = "リマインドの登録に失敗しました。"
+    else:
+        # それ以外はオウム返し
+        reply_msg = msg_from
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.source.user_id))
+        TextSendMessage(text=reply_msg))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.source.user_id))
 
 if __name__ == "__main__":
 #    app.run()
--- reminder/models/models.py
+++ reminder/models/models.py
@@ -4,39 +4,40 @@ from reminder.app import db
 class Base(db.Model):
     __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
     date_updated = db.Column(db.DateTime,  default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
 class Schedule(Base):
     __tablename__ = 'schedule'
 
+    user_id = db.Column(db.String(50))
     name = db.Column(db.String(30))
     message = db.Column(db.String(255))
     time = db.Column(db.DateTime, nullable=False)
 
     @classmethod
-    def create(cls, id, name, message, time):
+    def create(cls, user_id, name, message, time):
         """
         リマインドスケジュールを作成する
-        :param id:
+        :param user_id:
         :param name:
         :param message:
         :param time:
         :return: Schedule
         """
-        obj = cls(id=id, name=name, message=message, time=time)
+        obj = cls(id=id, user_id=user_id, name=name, message=message, time=time)
         db.session.add(obj)
         return obj
 
     @classmethod
-    def get_by_id(cls, id):
+    def get_by_id(cls, user_id):
         """
-        IDを指定して登録済みスケジュールを取得
-        :param id:
+        user_idを指定して登録済みスケジュールを取得
+        :param user_id:
         :return: ユーザScheduleリスト
         """
-        return db.session.query(cls).filter(cls.id == id).one_or_none()
+        return db.session.query(cls).filter(cls.user_id == user_id).one_or_none()
 
     # @classmethod
     # def get_all(cls):
@@ -47,13 +48,13 @@ class Schedule(Base):
     #     return db.session.query(cls).all()
 
     @classmethod
-    def delete_by_id(cls, id):
+    def delete_by_id(cls, user_id):
         """
-        IDを指定してスケジュール削除
-        :param id:
+        user_idを指定してスケジュール削除
+        :param user_id:
         :return: なし
         """
-        obj = db.session.query(cls).filter(cls.id == id).first()
+        obj = db.session.query(cls).filter(cls.user_id == user_id).first()
         db.session.delete(obj)
 
     @classmethod
@@ -67,12 +68,13 @@ class Schedule(Base):
         db.session.delete(obj)
 
 
-    def __init__(self, name, message, time):
+    def __init__(self, user_id, name, message, time):
+        self.user_id
         self.name = name
         self.message = message
         self.time = time
 
     def __repr__(self):
-        return 'Schedule(id={0}, name={1}, message={2}, time{3})'.format(
-            self.id, self.name, self.message, self.time
+        return 'Schedule(id={0}, user_id={1}, name={2}, message={3}, time{4})'.format(
+            self.id, user_id, self.name, self.message, self.time
         )
