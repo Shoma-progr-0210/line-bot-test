03e2fb974d0c58dfb83dccf6adba121357dbac75
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-02 23:31:14 +0900

    fix: app.pyでmodelsをインポート

--- reminder/app.py
+++ reminder/app.py
@@ -4,9 +4,12 @@ import os
 import logging
 import sys
 
+from .models import models
+
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # ログを標準出力に出力する
