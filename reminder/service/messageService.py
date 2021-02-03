4cd6d473c16ede09f6be5ac68ec7456fcf769cf0
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-04 00:15:11 +0900

    fix: メッセージフォーマット修正

--- reminder/service/messageService.py
+++ reminder/service/messageService.py
@@ -4,13 +4,13 @@ class MessageService():
     def create_message_from_list(self, schedules):
         reply_lines = []
         reply_lines.append("予定一覧")
-        reply_lines.append("---------------------")
+        reply_lines.append("----------------------------------")
 
         for row in schedules:
             reply_lines.append(f"予定名: {row['name']}")
             reply_lines.append(f"時間: {row['time']}")
             reply_lines.append(f"メッセージ: {row['message']}")
-            reply_lines.append("---------------------")
+            reply_lines.append("----------------------------------")
 
         return "\n".join(reply_lines)
         
