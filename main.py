0404525d7ec9c4346698f8882b581898151e9446
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-03 23:10:48 +0900

    fix: 修正

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def handle_message(event):
         profile = line_bot_api.get_profile(event.source.user_id)
         schedules = Schedule.get_by_user_id(profile.user_id)
         schedule_schema = ScheduleSchema(many=True)
-        reply_msg = schedule_schema.jsonify(schedule_schema)
+        reply_msg = schedule_schema.jsonify(schedules)
     else:
         # それ以外はオウム返し
         reply_msg = msg_from
