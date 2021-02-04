38fd2515f44c6441e2aa952c3e2dd44c6a6f21ca
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-05 02:09:50 +0900

    fix

--- reminder/jobs/remindmessage.py
+++ reminder/jobs/remindmessage.py
@@ -4,10 +4,12 @@ import os
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-# from reminder.app import app
+from flask import current_app, logging
 from reminder.models.schedule import Schedule, ScheduleSchema
 from reminder.service.messageservice import MessageService
 
+
+logger = logging.create_logger(current_app)
 # 環境変数取得
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 
@@ -15,7 +17,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def remind_message():
     time_now = datetime.now().replace(microsecond=0).replace(second=0) + timedelta(hours=9)
-    # app.logger.info(f"reminds start. time now => {time_now}")
+    logger.info(f"reminds start. time now => {time_now}")
     remind_schedules = Schedule.get_by_time(time_now)
     schedule_schema = ScheduleSchema(many=True)
     message_service = MessageService()
@@ -24,4 +26,4 @@ def remind_message():
     for user_id, msg in remind_msgs.items():
         line_bot_api.push_message(user_id, messages=msg)
 
-    # app.logger.info(f"reminds done. total count => {len(remind_msgs.keys())}")
+    logger.info(f"reminds done. total count => {len(remind_msgs.keys())}")
