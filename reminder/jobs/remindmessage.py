43e7f4f9906059d894c7629caa38d592fd632c69
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-04 23:38:44 +0900

    fix

--- reminder/jobs/remindmessage.py
+++ reminder/jobs/remindmessage.py
@@ -1,7 +1,2 @@
-import click
-from flask.cli import with_appcontext
-
-@click.command('remind', help="remind message")
-@with_appcontext
 def remind_message():
     print("hello world!!!")
--- reminder/remind.py
+++ reminder/remind.py
@@ -2,12 +2,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from reminder.jobs.remindmessage import remind_message
 
 
+sched = BlockingScheduler(daemon=True)
+
 def remind_job():
     print('This job is run every a minute.')
     remind_message()
 
 
 def sched_start():
-    sched = BlockingScheduler(daemon=True)
-    sched.add_job(remind_job, 'interval', minute=1)
+    sched.add_job(remind_job, 'interval', minutes=1)
     sched.start()
