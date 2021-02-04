cc273e17c7b24ed9079cbdc54edc24afca482491
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-05 01:33:59 +0900

    fix: 時間取得修正

--- reminder/jobs/remindmessage.py
+++ reminder/jobs/remindmessage.py
@@ -14,7 +14,7 @@ CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def remind_message():
-    time_now = datetime.datetime.now().replace(microsecond=0).replace(second=0)
+    time_now = datetime.now().replace(microsecond=0).replace(second=0) - timedelta(hours=9)
     remind_schedules = Schedule.get_by_time(time_now)
     schedule_schema = ScheduleSchema(many=True)
     message_service = MessageService()
