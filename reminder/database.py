9f80ebd14a03a30315b1d02561f2bfff43e4f3e5
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-05 02:22:28 +0900

    fix

--- reminder/database.py
+++ reminder/database.py
@@ -5,6 +5,5 @@ db = SQLAlchemy()
 ma = Marshmallow()
 
 def init_db(app):
-    with app.app_context():
-        db.init_app(app)
-        ma.init_app(app)
+    db.init_app(app)
+    ma.init_app(app)
