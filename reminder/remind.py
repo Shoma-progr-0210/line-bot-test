e007e966fec4f5f6a0f6b3b4d433d7e7a7c4373b
shoma.h <Shoma.progr.0210@gmail.com>
2021-02-05 00:43:40 +0900

    fix

--- reminder/remind.py
+++ reminder/remind.py
@@ -4,17 +4,11 @@ from reminder.jobs.remindmessage import remind_message
 
 class Config(object):
     SCHEDULER_API_ENABLED = True
-    JOBS = [
-        {
-            'id': 'remind_job',
-            'func': 'advanced:remind_job',
-            'trigger': 'interval',
-            'seconds': 60
-        }
-    ]
 
 scheduler = APScheduler()
 
+# cron examples
+@scheduler.task('cron', id='remind_job', minute='*')
 def remind_job():
     print('This job is run every a minute.')
     remind_message()
@@ -22,6 +16,6 @@ def remind_job():
 
 def scheduler_start(app):
     app.config.from_object(Config())
-    scheduler.init_app(app)
     # scheduler.api_enabled = True
+    scheduler.init_app(app)
     scheduler.start()
